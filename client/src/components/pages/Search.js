aeba44ce49bfa5669f6ec14c6593ff137b17ca79
landycodes <landryandrewsk8@gmail.com>
2023-06-06 15:37:52 -0700

    added styling and errorhandling on scrapper

--- client/src/components/pages/Search.js
+++ client/src/components/pages/Search.js
@@ -10,9 +10,11 @@ export default function Search() {
 
   return (
     <div className="d-flex flex-column align-items-center">
-      <h1>REI data Scrapper</h1>
+      <h1 className="mt-4">REI data Scrapper</h1>
       <input id="location" placeholder="Enter city and state" />
-      <button className="w-25 mt-2">+</button>
+      <button className="btn btn-light w-25 mt-2 p-0">
+        <h4>+</h4>
+      </button>
       <button
         className="w-50 mt-3"
         onClick={() => {
@@ -24,17 +26,62 @@ export default function Search() {
       </button>
 
       {getData ? (
-        <ul>
-          <li>Location: {getData.Location}</li>
-          <li>House growth value: {getData.MHV_Growth}</li>
-          <li>House value: {getData.MedianHouseValue}</li>
-          <li>Percentage of renters: {getData.PercentOfRenters}</li>
-          <li>Population: {getData.Population}</li>
-          <li>Population growth: {getData.PopulationGrowth}</li>
-          <li>Average 3 bed cost: {getData.ThreeBedRent}</li>
-          <li>Unemployment rate: {getData.Unemployment}</li>
-          <li>Vacancy rate: {getData.VacancyRate}</li>
-        </ul>
+        getData.Location ? (
+          <table className="table table-dark bg-dark mt-3 border border-white">
+            <tbody className="p-2">
+              <tr className="border border-white">
+                <th>{getData.Location}</th>
+              </tr>
+              <tr>
+                <td className="border border-white">House growth value </td>
+                <td className="border border-white">{getData.MHV_Growth}</td>
+              </tr>
+              <tr>
+                <td className="border border-white">House value </td>
+                <td className="border border-white">
+                  {getData.MedianHouseValue}
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white">Percentage of renters </td>
+                <td className="border border-white">
+                  {getData.PercentOfRenters}
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white">Population </td>
+                <td className="border border-white">{getData.Population}</td>
+              </tr>
+              <tr>
+                <td className="border border-white">Population growth </td>
+                <td className="border border-white">
+                  {getData.PopulationGrowth}
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white">Average 3 bed cost </td>
+                <td className="border border-white">{getData.ThreeBedRent}</td>
+              </tr>
+              <tr>
+                <td className="border border-white">Unemployment rate </td>
+                <td className="border border-white">{getData.Unemployment}</td>
+              </tr>
+              <tr>
+                <td className="border border-white">Vacancy rate </td>
+                <td className="border border-white">{getData.VacancyRate}</td>
+              </tr>
+            </tbody>
+          </table>
+        ) : (
+          <table className="table table-dark bg-dark mt-3 border border-white">
+            <tbody>
+              <tr>
+                <th className="border border-white"> {getData.search}</th>
+                <th className="border border-white"> {getData.uhOh}</th>
+              </tr>
+            </tbody>
+          </table>
+        )
       ) : (
         ""
       )}
--- server/utils/scrapper.js
+++ server/utils/scrapper.js
@@ -14,84 +14,97 @@ const getDataFor = async (search) => {
     page.click("#btnSearch"),
   ]);
 
-  //query selectors drills into page via css selectors and grabs data in the text
-  const data = await page.evaluate(() => {
-    const rowData = document.querySelector(".row:nth-child(5)");
-    const boxData = rowData.querySelectorAll("div > p");
-    const population = boxData[1].innerText;
-    const popGrowth = boxData[2].innerText;
-    const unEmploy = boxData[4].innerText;
-    const MHV = boxData[6].innerText;
+  const titleElement = await page.$("p.card-title.text-center");
+  if (!titleElement) {
+    browser.close();
+    return {
+      search: search,
+      uhOh: "Search failed, try again",
+    };
+  } else {
+    //query selectors drills into page via css selectors and grabs data in the text
+    const data = await page.evaluate(() => {
+      const title = document.querySelector(
+        "p.card-title.text-center"
+      ).textContent;
+      const rowData = document.querySelector(".row:nth-child(5)");
+      const boxData = rowData.querySelectorAll("div > p");
+      const population = boxData[1].innerText;
+      const popGrowth = boxData[2].innerText;
+      const unEmploy = boxData[4].innerText;
+      const MHV = boxData[6].innerText;
 
-    //all data collected from this page added to object
-    const dataObj = {
-      Population: population,
-      PopulationGrowth: popGrowth,
-      MedianHouseValue: MHV,
-      Unemployment: unEmploy,
+      //all data collected from this page added to object
+      const dataObj = {
+        Location: title,
+        Population: population,
+        PopulationGrowth: popGrowth,
+        MedianHouseValue: MHV,
+        Unemployment: unEmploy,
+      };
+
+      return dataObj;
+    });
+
+    //click home stats page and wait for content to load
+    await Promise.all([
+      page.waitForNavigation({ waitUntil: "domcontentloaded" }),
+      page.click(".list-group > li:nth-child(17) > a"),
+    ]);
+
+    //reads content on home stats page and returns data for object
+    const homeData = await page.evaluate(() => {
+      const tableData = document.querySelector(
+        ".table-responsive > table > tbody"
+      );
+
+      const houseGrowth = tableData.querySelector(
+        "tr:nth-child(5) > td:nth-child(2)"
+      ).innerText;
+
+      const threeBedAv = document.querySelector(
+        "g:nth-child(2) > text > tspan"
+      ).textContent;
+
+      const VacancyRate = tableData.querySelector(
+        "tr:nth-child(14) > td:nth-child(2)"
+      ).innerText;
+
+      const renterPop = document
+        .querySelector("h6.mt-3.mb-0")
+        .nextSibling.textContent.replace(/[^0-9.%]/g, "")
+        .trim();
+
+      const dataObj = {
+        MHV_Growth: houseGrowth,
+        ThreeBedRent: threeBedAv,
+        VacancyRate: VacancyRate,
+        PercentOfRenters: renterPop,
+      };
+
+      return dataObj;
+    });
+
+    await browser.close();
+
+    const reiData = {
+      Location: data.Location,
+      Population: data.Population,
+      PopulationGrowth: data.PopulationGrowth,
+      MedianHouseValue: data.MedianHouseValue,
+      MHV_Growth: homeData.MHV_Growth,
+      ThreeBedRent: homeData.ThreeBedRent,
+      VacancyRate: homeData.VacancyRate,
+      PercentOfRenters: homeData.PercentOfRenters,
+      Unemployment: data.Unemployment,
     };
 
-    return dataObj;
-  });
-
-  //click home stats page and wait for content to load
-  await Promise.all([
-    page.waitForNavigation({ waitUntil: "domcontentloaded" }),
-    page.click(".list-group > li:nth-child(17) > a"),
-  ]);
-
-  //reads content on home stats page and returns data for object
-  const homeData = await page.evaluate(() => {
-    const tableData = document.querySelector(
-      ".table-responsive > table > tbody"
-    );
-
-    const houseGrowth = tableData.querySelector(
-      "tr:nth-child(5) > td:nth-child(2)"
-    ).innerText;
-
-    const threeBedAv = document.querySelector(
-      "g:nth-child(2) > text > tspan"
-    ).textContent;
-
-    const VacancyRate = tableData.querySelector(
-      "tr:nth-child(14) > td:nth-child(2)"
-    ).innerText;
-
-    const renterPop = document
-      .querySelector("h6.mt-3.mb-0")
-      .nextSibling.textContent.replace(/[^0-9.%]/g, "")
-      .trim();
-
-    const dataObj = {
-      MHV_Growth: houseGrowth,
-      ThreeBedRent: threeBedAv,
-      VacancyRate: VacancyRate,
-      PercentOfRenters: renterPop,
-    };
-
-    return dataObj;
-  });
-
-  await browser.close();
-
-  const reiData = {
-    Location: search,
-    Population: data.Population,
-    PopulationGrowth: data.PopulationGrowth,
-    MedianHouseValue: data.MedianHouseValue,
-    MHV_Growth: homeData.MHV_Growth,
-    ThreeBedRent: homeData.ThreeBedRent,
-    VacancyRate: homeData.VacancyRate,
-    PercentOfRenters: homeData.PercentOfRenters,
-    Unemployment: data.Unemployment,
-  };
-
-  return reiData;
+    return reiData;
+  }
 };
 
 // export default getDataFor;
 module.exports = getDataFor;
 
 //call function to test scrapper
-// getDataFor("tucson az").then((data) => console.log(data));
+// getDataFor("san diego").then((data) => console.log(data));
