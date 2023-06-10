68df3610786a7701a9db65eb7712fdf1fdc5f379
landycodes <landryandrewsk8@gmail.com>
2023-06-09 19:25:13 -0700

    back to networkidle0

--- server/utils/scrapper.js
+++ server/utils/scrapper.js
@@ -81,7 +81,7 @@ const getDataFor = async (search) => {
       //click home stats page and wait for content to load
       console.log("clicking homeStats page....");
       await Promise.all([
-        page.waitForNavigation({ waitUntil: "domcontentloaded" }),
+        page.waitForNavigation({ waitUntil: "networkidle0" }),
         page.waitForSelector("ul.list-group > li:nth-child(17) > a"),
         page.click("ul.list-group > li:nth-child(17) > a"),
       ]);
