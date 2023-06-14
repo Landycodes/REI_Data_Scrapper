46459d60bb8b8c96f4e13765373b870607213638
landycodes <landryandrewsk8@gmail.com>
2023-06-13 18:35:01 -0700

    save tables via localstorage, added copy button

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Table from "./components/pages/Search";
 
 export default function App() {
+  document.title = "REI_Scrapper";
   return (
     <div id="container">
       <Table />
--- client/src/components/pages/Search.js
+++ client/src/components/pages/Search.js
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "../style.css";
 import { lookUp, pingServer } from "../../utils/API";
 import Loading from "../loading";
 
 export default function Search() {
-  const [getData, setData] = useState([]);
+  const [getData, setData] = useState(
+    localStorage.getItem("tables")
+      ? JSON.parse(localStorage.getItem("tables"))
+      : []
+  );
   const [inputs, addInput] = useState([]);
   const [loading, setLoad] = useState(false);
 
@@ -12,6 +16,12 @@ export default function Search() {
     pingServer();
   }, []);
 
+  //save tables before refresh
+  window.addEventListener("beforeunload", function () {
+    localStorage.setItem("tables", JSON.stringify(getData));
+  });
+
+  //adds response object to usestate hook
   const addObj = (object) => {
     const exists = getData.some((data) => data.Location === object.Location);
     if (!exists) {
@@ -21,6 +31,7 @@ export default function Search() {
     }
   };
 
+  //expands array of input elements
   const handleInputs = () => {
     addInput([
       ...inputs,
@@ -32,34 +43,48 @@ export default function Search() {
     ]);
   };
 
+  //deletes object by its index
   const handleDelete = (index) => {
     setData(getData.filter((item, i) => i !== index));
+    localStorage.setItem("tables", JSON.stringify(getData));
   };
 
-  // const testObj = {
-  //   Location: "Tucson, Arizona ",
-  //   Population: "538,167",
-  //   PopulationGrowth: "+2.4% since 2020",
-  //   MedianHouseValue: "$37,149",
-  //   MHV_Growth: "13.3%",
-  //   ThreeBedRent: "$1,700​",
-  //   VacancyRate: "3.3%",
-  //   PercentOfRenters: "44.4%",
-  //   Unemployment: "7.4%",
-  // };
-  // const testObj2 = {
-  //   Location: " new Tucson, Arizona ",
-  //   Population: "538,167",
-  //   PopulationGrowth: "+2.4% since 2020",
-  //   MedianHouseValue: "$37,149",
-  //   MHV_Growth: "13.3%",
-  //   ThreeBedRent: "$1,700​",
-  //   VacancyRate: "3.3%",
-  //   PercentOfRenters: "44.4%",
-  //   Unemployment: "7.4%",
-  // };
-  // addObj(testObj);
-  // addObj(testObj2);
+  const tableRef = useRef(null);
+
+  const copyTable = () => {
+    const table = tableRef.current;
+    const range = document.createRange();
+    const sel = window.getSelection();
+    const rows = table.rows;
+    console.log(table);
+    console.log(rows);
+
+    sel.removeAllRanges();
+
+    for (let i = 0; i < rows.length; i++) {
+      const cells = rows[i].cells;
+
+      for (let j = 0; j < cells.length; j++) {
+        const cell = cells[j];
+        console.log(cell);
+        // Check if the cell is part of the button column
+        if (!cell.classList.contains("ignoreCopy")) {
+          range.selectNodeContents(cell);
+          sel.addRange(range);
+        }
+      }
+    }
+
+    try {
+      const successful = document.execCommand("copy");
+      const message = successful ? "Table copied!" : "Unable to copy table.";
+      console.log(message);
+    } catch (err) {
+      console.error("Failed to copy table:", err);
+    }
+
+    sel.removeAllRanges();
+  };
 
   return (
     <div className="d-flex flex-column align-items-center">
@@ -113,35 +138,9 @@ export default function Search() {
                 addInput([]);
                 const target = document.querySelector(".add");
                 target.style.display = "block";
+                console.log(JSON.stringify(getData));
               });
           }
-
-          //runs one at a time
-          // for (const item of search) {
-          //   try {
-          //     const data = await lookUp(item.value);
-          //     console.log(data);
-          //     addObj(data);
-          //   } catch (error) {
-          //     console.error(error);
-          //   }
-          // }
-
-          //runs all of them simultaneously
-          // search.forEach(async (item) => {
-          //   await lookUp(item.value)
-          //     .then((data) => {
-          //       console.log(data);
-          //       addObj(data);
-          //     })
-          //     .finally(() => {
-          //       setLoad(false);
-          //       item.value = "";
-          //       addInput([]);
-          //       const target = document.querySelector(".add");
-          //       target.style.display = "block";
-          //     });
-          // });
         }}
       >
         Search
@@ -152,13 +151,20 @@ export default function Search() {
           ? getData.map((data, index) => {
               return data.Location ? (
                 <table
+                  ref={tableRef}
                   key={index}
                   className="table table-dark bg-dark mt-3 border border-white"
                 >
                   <tbody className="p-2">
                     <tr className="border border-white">
                       <th>{data.Location}</th>
-                      <th>
+                      <th className="ignoreCopy">
+                        <button
+                          className="btn btn-secondary btn-sm m-0 float-start"
+                          onClick={() => copyTable()}
+                        >
+                          Copy
+                        </button>
                         <button
                           className="btn btn-danger btn-sm m-0 float-end"
                           onClick={() => handleDelete(index)}
@@ -169,7 +175,7 @@ export default function Search() {
                     </tr>
                     <tr>
                       <td className="border border-white">
-                        House growth value{" "}
+                        House growth value
                       </td>
                       <td className="border border-white">{data.MHV_Growth}</td>
                     </tr>
