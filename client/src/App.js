0982fd65c6881a26f0ca3f576a68ea6efca600a7
landycodes <landryandrewsk8@gmail.com>
2023-06-18 15:46:44 -0700

    troubleshooting build fail on vercel

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Table from "./components/pages/MainPage";
-import VersionOne from "./components/pages/VersionOne";
 
 export default function App() {
   document.title = "REI_Scrapper";
